93d4e7a66639bdb3bf15a965c68d76dba0946174
salah <salah1999jari@gmail.com>
2026-03-20 10:03:51 +0100

    push

--- check_mongo.py
+++ check_mongo.py
@@ -8,7 +8,7 @@ anomaly_docs = set()
 for a in db.anomalies.find():
     for d_id in a.get("document_ids", []):
         anomaly_docs.add(d_id)
-
+ 
 print(f"Total curated: {len(curated_ids)}")
 print(f"Total anomalies docs count: {db.anomalies.count_documents({})}")
 print(f"Unique Docs with anomaly: {len(anomaly_docs)}")
